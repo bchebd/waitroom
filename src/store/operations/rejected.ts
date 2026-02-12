f16cf91496d997cbca1ac9cfdef18d4d39d163a1
Daniil Cheban <bchebd@bbcag.ch>
2026-02-12 15:35:49 +0100

    final: 1.0.1

--- src/store/operations/rejected.ts
+++ src/store/operations/rejected.ts
@@ -1,3 +1,3 @@
-export const REJECTED = (thunkAPI, e) => thunkAPI.rejectWithValue(
+export const REJECTED = (thunkAPI: any, e: any) => thunkAPI.rejectWithValue(
         e.response?.data?.message ?? e.message ?? "Unknown error",
     );
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/waitroom/",
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
